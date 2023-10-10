ecf3b0c3dd317ffc005336cf0157085b83ab63e9
christian.boe.erisken <christian.boe.eriksen@gmail.com>
2023-10-10 14:27:00 +0200

    Done with all tasks

--- iterations.r
+++ iterations.r
@@ -45,23 +45,34 @@ stations_metadata_df <-
 source("functions/data_tests.r")
 test_stations_metadata(stations_metadata_df)
 
+### 4a: time-function
+source("functions/data_transformations.r")
+
+# Test the function
+to_iso8601(as_datetime("2016-09-01 10:11:12"),0)
+to_iso8601(as_datetime("2016-09-01 10:11:12"),-4)
 
 ### 5: Final volume query: 
-
 source("gql-queries/vol_qry.r")
 
+# Extract 1 random station
 stations_metadata_df %>% 
   filter(latestData > Sys.Date() - days(7)) %>% 
-  sample_n(1) %$% 
-  vol_qry(
-    id = id,
-    from = to_iso8601(latestData, -4),
-    to = to_iso8601(latestData, 0)
+  sample_n(1) -> randome_station
+  
+# Query the data, and make a line plot over the volume
+vol_qry(
+    id = randome_station$id,
+    from = to_iso8601(randome_station$latestData, -4),
+    to = to_iso8601(randome_station$latestData, 0)
   ) %>% 
   GQL(., .url = configs$vegvesen_url) %>%
-  transform_volumes() %>% 
+  transform_volumes() %>%  
   ggplot(aes(x=from, y=volume)) + 
   geom_line() + 
+  xlab("Dates") +
+  ylab("Volume") +
+  ggtitle(paste("Traffic during a 4 days timeperiode at", randome_station$name )) +
   theme_classic()
 
 
